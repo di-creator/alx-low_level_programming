9b4544bb103619db3483e0735b4abef69553baa3
di-creator <yorkedianaba@gmail.com>
2023-11-06 16:21:19 -0800

    gcc -std=gnu89 main.c -L. -lmy -o quote

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - confirm  uppercase letters
+ * @c: char to confirm
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"I have learnt to love programming overtime");
+	return (0);
+}
