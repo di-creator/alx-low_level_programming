4dc2a8d7fd201de016dd355e70b41bf05f8be61d
di-creator <131366061+di-creator@users.noreply.github.com>
2023-12-15 03:09:32 +0100

    Create 0-binary_uint.c

---
+++ 0x14-bit_manipulation/0-binary_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+ * binary_to_uint: Converts a binary string 
+ * Parameters: b - Binary string 
+ * Returns: Converted unsigned integer. 
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
+}
